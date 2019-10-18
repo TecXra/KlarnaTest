c94b62f63fb76536c5d0ce61dffff20889f8a097
abrarkiyani <36422775+abrarkiyani@users.noreply.github.com>
2019-10-18 14:04:58 +0200

    Add files via upload

---
+++ config/mail-old2.php
@@ -0,0 +1,19 @@
+<?php
+
+// return [
+//   'driver' => 'smtp',
+//   'host' => 'vps.local.12345',
+//   'port' => 587,
+//   'encryption' => null,
+//   'username' => 'order@wheelzone.se',
+//   'password' => 'zZ~oE.d[KG',
+// ];
+
+return [
+  'driver' => 'mail',
+  'host' => 'vps.local.12345',
+  'port' => 587,
+  'encryption' => 'tls',
+  'username' => 'no-reply@wheeldata.se',
+  'password' => 'm]2cdNVi3suo',
+];
